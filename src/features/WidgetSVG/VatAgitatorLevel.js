1d2fb5214a58a3fe8caf4b36f4830582126b1ccc
pasindu-iclick <pasindu@leadlanka.lk>
2025-08-12 23:18:49 +0530

    add level

--- src/features/WidgetSVG/VatAgitatorLevel.js
+++ src/features/WidgetSVG/VatAgitatorLevel.js
@@ -1,26 +1,8 @@
-const VatAgitatorMixser = `<?xml version="1.0" ?>
-<svg width="112" height="480" viewBox="0 0 112 480" fill="none" xmlns="http://www.w3.org/2000/svg">
-<rect x="56" width="18" height="352" fill="url(#paint0_linear_43_115)"/>
-<rect x="49" y="352" width="32" height="29" fill="url(#paint1_linear_43_115)"/>
-<path d="M7.32259 395.911L49.5 381H80.5L58.9737 392.908C57.9241 393.489 57.6181 394.856 58.3202 395.829L107.459 463.902C109.451 466.662 108.164 470.561 104.921 471.593L79.6845 479.623C78.9592 479.854 78.1657 479.653 77.6377 479.105L5.388 404.094C2.865 401.474 3.89369 397.123 7.32259 395.911Z" fill="url(#paint2_linear_43_115)"/>
-<defs>
-<linearGradient id="paint0_linear_43_115" x1="56" y1="176" x2="74" y2="176" gradientUnits="userSpaceOnUse">
-<stop stop-color="#737373"/>
-<stop offset="0.514423" stop-color="#D9D9D9"/>
-<stop offset="1" stop-color="#737373"/>
-</linearGradient>
-<linearGradient id="paint1_linear_43_115" x1="49" y1="366.5" x2="81" y2="366.5" gradientUnits="userSpaceOnUse">
-<stop stop-color="#737373"/>
-<stop offset="0.514423" stop-color="#DDDDDD"/>
-<stop offset="1" stop-color="#737373"/>
-</linearGradient>
-<linearGradient id="paint2_linear_43_115" x1="55.75" y1="366" x2="55.75" y2="480" gradientUnits="userSpaceOnUse">
-<stop stop-color="#737373"/>
-<stop offset="1" stop-color="#D9D9D9"/>
-</linearGradient>
-</defs>
+const VatAgitatorLevel = `<?xml version="1.0" ?>
+<svg width="172" height="513" viewBox="0 0 172 513" fill="none" xmlns="http://www.w3.org/2000/svg">
+<rect width="172" height="513" rx="15" fill="#BCD9DF"/>
 </svg>
 
 `;
 
-export default VatAgitatorMixser;
+export default VatAgitatorLevel;
